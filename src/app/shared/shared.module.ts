5034166dbf7b8a23623bfb7e291b69a02371bda2
rubikal23 <paritapatel1995@gmail.com>
2019-08-12 05:12:53 +0530

    add login component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared.component';
 
+import { LoginModule } from "../login/login.module";
+
 @NgModule({
   declarations: [SharedComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginModule,
   ]
 })
 export class SharedModule { }
